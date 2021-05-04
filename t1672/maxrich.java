c12ceacb52c6c31da10ee552e2604836ba592313
mytangyh <1450521339@qq.com>
2021-05-04 22:06:54 +0800

    1672. 最富有客户的资产总量

---
+++ t1672/maxrich.java
@@ -0,0 +1,23 @@
+public class maxrich {
+    public static void main(String[] args) {
+        int[][] accounts={{2,8,7},{7,1,3},{1,9,5}};
+        System.out.println(maximumWealth(accounts));
+    }
+
+    public static int maximumWealth(int[][] accounts) {
+        int max=0;
+        for (int i = 0; i < accounts.length; i++) {
+            int sum=0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum+=accounts[i][j];
+              
+            }
+            if (sum>max) {
+                max=sum;
+            }
+        }
+        
+        return max;
+
+    }
+}
